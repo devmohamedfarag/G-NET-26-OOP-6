df11fdb11ffacae92cedc322392681c903b2504b
mohamedfarag22-dot <mohamedfarag2272001@gmail.com>
2026-03-12 13:57:31 +0200

    Question01:

--- AssignmentOOP06/Program.cs
+++ AssignmentOOP06/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Qusetion01:
+// Q1: What is abstraction in OOP?
+//     How is it different from encapsulation?
+//     Give a real-world example (not from the session) that shows the difference between the two.
+
+// Answer: Abstraction is the concept of showing only the essential features of an object while hiding the complex implementation details.
+//         It focuses on what an object does, not how it does it.
+//         Encapsulation, on the other hand, is the concept of bundling data and methods that operate on that data within a single unit (class) and restricting access to some of the object's components.
+//         It focuses on how an object is implemented and protects the internal state of the object from outside interference.
+/*
+          Real-World Example (ATM Machine)
+         
+          * Abstraction:
+          When you use an ATM machine, you only see options like:
+         - Withdraw money
+         - Check balance
+         - Deposit money
+          You don’t see how the bank processes the transaction internally.
+          The complex system is hidden. This is abstraction.
+          
+          * Encapsulation
+          Inside the bank system, your account balance is stored privately.
+          You cannot directly change it. Instead, you must use operations like:
+          - Withdraw
+          - Deposit
+          These operations control how the balance changes.
+          This is encapsulation.
+ */
+
+#endregion
